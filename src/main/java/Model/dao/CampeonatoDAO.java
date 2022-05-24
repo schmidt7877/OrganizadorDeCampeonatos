39b3f630ebe591ce0ebf45130b721de86a5a1809
schmidt7877 <cmateus777@gmail.com>
2022-05-24 01:25:09 -0300

    metodo create campeonatoDAO

--- src/main/java/Model/dao/CampeonatoDAO.java
+++ src/main/java/Model/dao/CampeonatoDAO.java
@@ -4,10 +4,42 @@
  */
 package Model.dao;
 
+import Connection.ConnectionFactory;
+import Models.Campeonato;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author cmate
  */
 public class CampeonatoDAO {
+   
+    public void create(Campeonato campeonato){
+        
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("INSERT INTO campeonato (nome,usuario_id,data)VALUES(?,?,?)");
+            stmt.setString(1, campeonato.getNome());
+            stmt.setInt(2, campeonato.getUsuarioId());
+            
+           
+            stmt.executeUpdate();
+            
+            
+            JOptionPane.showMessageDialog(null, "Cadastro efetuado com Sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+            
+        }
+        
+        
+    }
     
 }
