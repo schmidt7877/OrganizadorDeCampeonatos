cc2769d7d24e83e121d4a0991dfb199740d6b344
Arley Novais do Nascimento <nascimentoarle@gmail.com>
2022-06-07 21:29:17 -0300

    Create Vizualizar Times

--- src/main/java/View/FrameMenuPrincipal.java
+++ src/main/java/View/FrameMenuPrincipal.java
@@ -160,7 +160,9 @@ public class FrameMenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        FrameVisualizarTimes tela = new FrameVisualizarTimes();
+        tela.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
